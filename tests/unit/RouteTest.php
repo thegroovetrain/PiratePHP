83244775d18039b68a4db8c4e379579a5e1e464f
Eric Seibt <ericthegroovetrain@gmail.com>
2023-11-14 17:57:29 -0500

    mostly response refactor

---
+++ tests/unit/RouteTest.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use thegroovetrain\PiratePHP\Route;
+
+
+final class RouteTest extends MockeryTestCase
+{
+    public function testRegex():void
+    {
+        
+        
+    }
+}
